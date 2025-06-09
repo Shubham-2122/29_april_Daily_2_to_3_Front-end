2416345b965aa1e21f079f9801178e6ce292c8f5
Shubham-2122 <shubhamjadav.tops@gmail.com>
2025-06-09 14:47:27 +0530

    js

---
+++ JavaScript/ex.js
@@ -0,0 +1,10 @@
+console.log("Hello this External file");
+
+document.write("Hello dom ","<br>")
+document.writeln(10+10)
+
+console.log(document.getElementById("h1"))
+document.getElementById("h1").innerText = "<i> HEllO </i>";
+document.getElementById("h1").innerHTML = "<i> HEllO </i>"
+
+// javscript Code line by line
