d373e28d2977f044620b95c59ceb6b17ce0912c9
Shubham-2122 <shubhamjadav.tops@gmail.com>
2025-06-10 15:02:50 +0530

    js

--- JavaScript/ex.js
+++ JavaScript/ex.js
@@ -1,10 +1,16 @@
-console.log("Hello this External file");
+// console.log("Hello this External file");
 
-document.write("Hello dom ","<br>")
-document.writeln(10+10)
-
+// document.write("Hello dom ","<br>")
+// // document.writeln(10+10)
+// 
 console.log(document.getElementById("h1"))
 document.getElementById("h1").innerText = "<i> HEllO </i>";
 document.getElementById("h1").innerHTML = "<i> HEllO </i>"
 
 // javscript Code line by line
+
+// Bom :- browser object model :- for function
+// alert("pls filed required")
+// confirm("Are you sure ?")
+// prompt("5 + 5 => ?")
+// print()
